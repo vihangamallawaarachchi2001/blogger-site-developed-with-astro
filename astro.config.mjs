2111ceb57c193053b7212bab5afef6dc0d41020c
vihanga <vihanganethusara00@gmail.com>
2025-11-30 23:07:15 +0530

    feat: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'http://localhost:4321/',
+
+  integrations: [mdx({}), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
